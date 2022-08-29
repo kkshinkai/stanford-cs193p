9badaf5f472aedab1c8f9724481f7c2c231821e3
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-08-29 23:59:53 +0800

    Initialize the Memorize game project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kk Shinkai on 2022/8/29.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
