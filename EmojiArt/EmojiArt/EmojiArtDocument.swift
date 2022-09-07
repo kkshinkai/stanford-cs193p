2d8f110676e3e59f54a3df0436f89097ad9a19ee
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-09-07 22:45:31 +0800

    Lecture 9: EmojiArt drag and drop

---
+++ EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    @Published private(set) var emojiArt: EmojiArtModel
+    
+    init() {
+        emojiArt = EmojiArtModel()
+        emojiArt.addEmoji("🏀", at: (-200, -100), size: 80)
+        emojiArt.addEmoji("⚽️", at: (-50, 100), size: 40)
+    }
+    
+    var emojis: [EmojiArtModel.Emoji] { emojiArt.emojis }
+    var background: EmojiArtModel.Background { emojiArt.background }
+    
+    // MARK: - Intent(s)
+    
+    func setBackground(_ background: EmojiArtModel.Background) {
+        emojiArt.background = background
+    }
+    
+    func addEmoji(_ emoji: String, at location: (x: Int, y: Int), size: CGFloat) {
+        emojiArt.addEmoji(emoji, at: location, size: Int(size))
+    }
+}
